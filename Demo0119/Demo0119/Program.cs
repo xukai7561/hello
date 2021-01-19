81f2ff90520468cf1f3a5cfd55dc970b7ae88faf
xukai7561 <17756227561@139.com>
2021-01-19 09:50:26 +0800

    1

---
+++ Demo0119/Demo0119/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Demo0119
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            if (a > b)
+            {
+                Console.WriteLine("value is true;");
+            }
+            else
+            {
+                Console.WriteLine("value is false;");
+            }
+            Console.ReadLine();
+        }
+    }
+}
